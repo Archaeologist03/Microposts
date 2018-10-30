9a120d06c1c56daceb9137af22f3892d5dbe61ae
Ilija <omniiknight@hotmail.com>
2018-10-30 21:30:26 +0100

    Done project - adding rest - edit state, delete...

--- src/ui.js
+++ src/ui.js
@@ -33,6 +33,80 @@ class UI {
 
         this.post.innerHTML = output;
     }
+
+    showAlert(message, className) {
+        this.clearAlert();
+
+        const div = document.createElement("div");
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector(".postsContainer");
+        const posts = document.querySelector("#posts");
+        container.insertBefore(div, posts);
+
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector(".alert");
+
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput.value = "";
+        this.bodyInput.value = "";
+
+    }
+
+    fillForm(data) {
+        this.titleInput.value = data.title;
+        this.bodyInput.value = data.body;
+        this.idInput.value = data.id;
+
+        this.changeFormState("edit");
+    }
+
+    //clear ID hidden value
+    clearIdInput() {
+        this.idInput.value = "";
+    }
+
+    //change form state
+    changeFormState(type) {
+        if(type === "edit") {
+            this.postSubmit.textContent = "Update post";
+            this.postSubmit.className = "post-submit btn btn-warning btn-block";
+
+            //create cancel btn
+            const button = document.createElement("button");
+            button.className = "post-cancel btn btn-light btn block";
+            button.appendChild(document.createTextNode("Cancel Edit"));
+
+            //get parent
+            const cardForm = document.querySelector(".card-form");
+            //get elm to insert before
+            const formEnd = document.querySelector(".form-end");
+            //insert cancel btn
+            cardForm.insertBefore(button, formEnd);
+        }
+        else {
+            this.postSubmit.textContent = "Post it";
+            this.postSubmit.className = "post-submit btn btn-primary btn-block";
+            // remove cancel btn if it is there
+            if(document.querySelector(".post-cancel")) {
+                document.querySelector(".post-cancel").remove()
+            }
+            //clear id from hidden field
+            this.clearIdInput();
+            //clear text
+            this.clearFields();
+        }
+    }
 }
 
 
